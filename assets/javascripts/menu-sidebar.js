58a48359e611291deef30dd8f677e4abfb3ca902
docupajak <docu.pajak@gmail.com>
2019-01-31 17:06:49 +0800

    card

---
+++ assets/javascripts/menu-sidebar.js
@@ -0,0 +1,24 @@
+//https://gist.github.com/berlamhenderson/48b68d3d0ad222fd0097b9d872795873
+
+const menuIconEl = $('.menu-icon');
+const sidenavEl = $('.sidenav');
+const sidenavCloseEl = $('.sidenav__close-icon');
+
+// Add and remove provided class names
+function toggleClassName(el, className) {
+  if (el.hasClass(className)) {
+    el.removeClass(className);
+  } else {
+    el.addClass(className);
+  }
+}
+
+// Open the side nav on click
+menuIconEl.on('click', function() {
+  toggleClassName(sidenavEl, 'active');
+});
+
+// Close the side nav on click
+sidenavCloseEl.on('click', function() {
+  toggleClassName(sidenavEl, 'active');
+});
